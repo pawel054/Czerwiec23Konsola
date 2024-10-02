82f3d9369885e1855c92545a8d6c94199aa34370
pawel054 <pawel.marcisz1@gmail.com>
2024-10-02 08:38:15 +0200

    Add line comment

--- AplikacjaKonsola/Program.cs
+++ AplikacjaKonsola/Program.cs
@@ -9,6 +9,16 @@
             FillArray(A);
         }
 
+
+        /* 
+        *******************************************************
+        nazwa funkcji: FillArray
+        parametry wejściowe: bool[] array - przechowuje wartosci true i false
+        wartość zwracana: brak
+        informacje: Funkcja wypełnia tablicę wartościami true, następnie szuka liczb pierwszych z przediału 2 do n wykorzystując algorytm sita Eratostenesa. Pod koniec wypisuje w konsoli liczby pierwsze z separatorem.
+        autor: Paweł Marcisz
+        *******************************************************
+        */
         private static void FillArray(bool[] array)
         {
             for(int i = 0; i < array.Length; i++)

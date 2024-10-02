6b3d94fa6bd8022695f5898383b90d7b4bb19e69
pawel054 <pawel.marcisz1@gmail.com>
2024-10-02 08:29:00 +0200

    Add FillArray function

--- AplikacjaKonsola/Program.cs
+++ AplikacjaKonsola/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int n = 100;
+            bool[] A = new bool[n+1];
+            FillArray(A);
+        }
+
+        private static void FillArray(bool[] array)
+        {
+            for(int i = 0; i < array.Length; i++)
+            {
+                array[i] = true;
+            }
+
+            for(int i = 2; i <= Math.Sqrt(array.Length-1); i++)
+            {
+                if (array[i] == true)
+                {
+                    for(int j = 2*i; j<= array.Length-1; j += i)
+                    {
+                        array[j] = false;
+                    }
+                }
+            }
         }
     }
 }
